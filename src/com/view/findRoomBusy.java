5f94d97367b53a7f0115e79c4e1a50cfe2d1cb35
NEXT Speed <NEXT Speed@192.168.99.1>
2017-09-18 22:26:05 +0700

    ..

--- src/com/view/findRoomBusy.java
+++ src/com/view/findRoomBusy.java
@@ -1,11 +1,20 @@
 package com.view;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
+import com.hotel.HotelServiceInterfaceProxy;
+import com.hotel.Room;
 
 /**
  * Servlet implementation class findRoomBusy
@@ -14,28 +23,56 @@ import javax.servlet.http.HttpServletResponse;
 public class findRoomBusy extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public findRoomBusy() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+    	response.setContentType("text/html;charset-UTF-8");
+    	PrintWriter out = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "UTF8"), true);
+    	
+    	// value from web
+    	String startDate = request.getParameter("startDate");
+    	String endDate = request.getParameter("endDate");
+
+    
+    	System.out.println(startDate+endDate);
+    	
+    	// connect soap
+    	HotelServiceInterfaceProxy soap = new HotelServiceInterfaceProxy();
+    	
+    	Room[] roomList = null;
+   
+    	roomList = soap.findRoomBusy(startDate, endDate);	
+    	
+		// out put web
+    	out.print("<html>");
+    	out.println("<head><base href=\"http://localhost:8080/HotelClient/\"></head>");
+		out.println("<body>");
+		
+		out.println("<table border=\"0\">");
+		
+				
+				for (com.hotel.Room room : roomList) {
+					out.println("<tr>");
+					out.println("<td>"+room.getId()+"</td>");
+					out.println("<td>"+room.getRoomID()+"</td>");
+					out.println("<td>"+room.getType()+"</td>");
+					out.println("<td>"+room.getPrice()+"</td>");
+					
+				}
+			
+		out.println("</table");
+		
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		processRequest(request, response);
 	}
 
 }
