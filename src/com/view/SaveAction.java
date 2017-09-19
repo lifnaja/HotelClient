33f88088949f061a7744d18c97d14097425fe446
SUDARAT\sp <sp.sudaratphiwon@hotmail.com>
2017-09-19 23:35:22 +0700

    updatesaveaction

--- src/com/view/SaveAction.java
+++ src/com/view/SaveAction.java
@@ -50,23 +50,32 @@ public class SaveAction extends HttpServlet {
 				
 		String output = "";
 		if (result == false) {
-			output = "Susess";
+			output = "ทำรายการไม่สำเร็จ";
 		}else {
-			output = "Un Susess";
+			output = "ทำรายการสำเร็จ";
 		}
 		
 		// out put web
 		out.print("<html>");
 		out.println("<head><base href=\"http://localhost:8080/HotelClient/\">");
 		out.println("<meta charset=\"utf-8\">");
+		out.println("<!-- Latest compiled and minified CSS -->\n" + 
+				"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\n" + 
+				"\n" + 
+				"<!-- Optional theme -->\n" + 
+				"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">\n" + 
+				"\n" + 
+				"<!-- Latest compiled and minified JavaScript -->\n" + 
+				"<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>");
+		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
 		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
 		out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css\" integrity=\"sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\" crossorigin=\"anonymous\">");
 		out.println("</head>");
 		out.print("<body>");
 		out.println("<center>");
 		out.println("<h2>ผลลัพทธ์</h2>");
-		out.println(output+"<br><br>");
-		out.println("<a href=\"allroom\">กลับสู่หน้าหลัก</a>");
+		out.println("<div class=\"alert alert-success\" role=\"alert\"><h3>"+output+"</h3></div><br><br>");
+		out.println("<button type=\"submit\" class=\"btn btn-primary\">กลับสู่หน้าหลัก</button>");
 		out.println("</center>");
 		
 		out.println("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>");
