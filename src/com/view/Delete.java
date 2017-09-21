c9321210980db1df3dfad6995717037ba5b4d9dd
DELL <DELL@172.16.33.133>
2017-09-21 21:23:47 +0700

    edit delete

--- src/com/view/Delete.java
+++ src/com/view/Delete.java
@@ -31,24 +31,35 @@ public class Delete extends HttpServlet {
     	
 		String output = "";
 		if (result == false) {
-			output = "Unsuccessful";
+			output = "ลบข้อมูลไม่สำเร็จ";
 		}else {
-			output = "Successful";
+			output = "ลบข้อมูลได้สำเร็จ";
 		}
 		
 		// out put web
 		out.print("<html>");
 		out.println("<head><base href=\"http://localhost:8080/HotelClient/\">");
 		out.println("<meta charset=\"utf-8\">");
+		out.println("<!-- Latest compiled and minified CSS -->\n" + 
+				"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\n" + 
+				"\n" + 
+				"<!-- Optional theme -->\n" + 
+				"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">\n" + 
+				"\n" + 
+				"<!-- Latest compiled and minified JavaScript -->\n" + 
+				"<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>");
+		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
 		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
 		out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css\" integrity=\"sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\" crossorigin=\"anonymous\">");
 		out.println("</head>");
 		out.print("<body>");
-		out.println("<center>");
-		out.println("<h2>เธ?เธฅเธฅเธฑเธ?เธ—เธ?เน?</h2>");
-		out.println(output+id+"<br><br>");
-		out.println("<a href=\"allroom\">Return Allroom รีเทิร์นกลับ  ออลรูม</a>");
-		out.println("</center>");
+		out.print("<div class=\"container\">");
+		
+		out.println("<h2>ผลลัพธ์</h2>");
+		out.println("<div class=\"alert alert-success\" role=\"alert\"><h3>"+output+"</h3></div><br><br>");
+		out.println("<a href=\"allroom\"><button type=\"button\" class=\"btn btn-primary\">กลับสู่หน้าหลัก</button></a>");
+		out.println("</div>");
+		
 		
 		out.println("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>");
 		out.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js\" integrity=\"sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4\" crossorigin=\"anonymous\"></script>");
